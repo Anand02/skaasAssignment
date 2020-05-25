7630536cff9479098309754115f4e909063c5a0d
Anand <anand.sg@fleximc.com>
2020-05-25 19:00:10 +0530

    Updates

---
+++ precache-manifest.b992c6871e981bac0cf3fec02e44b69b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d986441e626aec86d8a2af25095d25b",
+    "url": "/skaasAssignment/index.html"
+  },
+  {
+    "revision": "9da95210b11b6dd66573",
+    "url": "/skaasAssignment/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dc0a3b260c51ca4440b2",
+    "url": "/skaasAssignment/static/css/main.5928ade2.chunk.css"
+  },
+  {
+    "revision": "9da95210b11b6dd66573",
+    "url": "/skaasAssignment/static/js/2.74e86638.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skaasAssignment/static/js/2.74e86638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0a3b260c51ca4440b2",
+    "url": "/skaasAssignment/static/js/main.4fa24615.chunk.js"
+  },
+  {
+    "revision": "383f120d2e2f1f6974b6",
+    "url": "/skaasAssignment/static/js/runtime-main.d29517cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/skaasAssignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
